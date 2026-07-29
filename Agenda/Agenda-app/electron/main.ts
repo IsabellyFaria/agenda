f70a03f36c2c2740978354e250f5f672cff950c9
Isabelly Faria <isabelly.faria06@gmail.com>
2026-07-29 00:57:34 +0000

    Iniciando projeto

---
+++ Agenda/Agenda-app/electron/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.ts"),
+    },
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  } else {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
